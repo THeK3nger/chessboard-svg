4bb1989457106ed543331ffdb6de29d59284da32
Davide Aversa <thek3nger@gmail.com>
2021-02-26 14:55:15 +0100

    refactor: moving files around

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,116 @@
-import { SVGChessboard } from "./SVGChessboard";
+import { BoardCoordinate, Chessboard } from "./Chessboard";
 
-const example = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-const example2 = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
+export class SVGChessboard {
+  chessboard: Chessboard;
 
-/**
- * Example consumer function.
- */
-function drawChessboard(): SVGElement {
-  const xmlns = "http://www.w3.org/2000/svg";
-  var boxWidth = 500;
-  var boxHeight = 500;
-  var block = document.createElementNS(xmlns, "svg");
-  block.setAttributeNS(null, "viewBox", "0 0 " + boxWidth + " " + boxHeight);
-  block.setAttributeNS(null, "width", String(boxWidth));
-  block.setAttributeNS(null, "height", String(boxHeight));
+  private readonly xmlns = "http://www.w3.org/2000/svg";
+  private readonly whiteColor = "white";
+  private readonly blackColor = "gray";
 
-  const svgChessboard = SVGChessboard.fromFEN(example);
-  var board = svgChessboard.draw();
-
-  block.appendChild(board);
-  block.style.display = "block";
-
-  const svgContainer = document.getElementById("svgContainer");
-  if (svgContainer !== null) {
-    svgContainer.appendChild(block);
+  private constructor(
+    chessboard: Chessboard,
+    private readonly squareSize: number
+  ) {
+    this.chessboard = chessboard;
   }
-  return block;
-}
 
-drawChessboard();
+  draw(): SVGElement {
+    let g = document.createElementNS(this.xmlns, "g");
+    g.appendChild(this.drawBoard());
+    g.appendChild(this.drawPieces());
+    return g;
+  }
+
+  drawBoard(): SVGElement {
+    console.log("Creating Board");
+    let g = document.createElementNS(this.xmlns, "g");
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        g.appendChild(this.drawRect([i, j]));
+      }
+    }
+    return g;
+  }
+
+  drawPieces(): SVGElement {
+    let g = document.createElementNS(this.xmlns, "g");
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        const piece = this.chessboard.get(i, j);
+        if (piece === "K") {
+          g.appendChild(this.drawPiece([j, i], WHITE_KING));
+        }
+        if (piece === "Q") {
+          g.appendChild(this.drawPiece([j, i], WHITE_QUEEN));
+        }
+        if (piece === "N") {
+          g.appendChild(this.drawPiece([j, i], WHITE_KNIGHT));
+        }
+        if (piece === "R") {
+          g.appendChild(this.drawPiece([j, i], WHITE_ROOK));
+        }
+        if (piece === "B") {
+          g.appendChild(this.drawPiece([j, i], WHITE_BISHOP));
+        }
+        if (piece === "P") {
+          g.appendChild(this.drawPiece([j, i], WHITE_PAWN));
+        }
+        if (piece === "k") {
+          g.appendChild(this.drawPiece([j, i], BLACK_KING));
+        }
+        if (piece === "q") {
+          g.appendChild(this.drawPiece([j, i], BLACK_QUEEN));
+        }
+        if (piece === "n") {
+          g.appendChild(this.drawPiece([j, i], BLACK_KNIGHT));
+        }
+        if (piece === "r") {
+          g.appendChild(this.drawPiece([j, i], BLACK_ROOK));
+        }
+        if (piece === "b") {
+          g.appendChild(this.drawPiece([j, i], BLACK_BISHOP));
+        }
+        if (piece === "p") {
+          g.appendChild(this.drawPiece([j, i], BLACK_PAWN));
+        }
+      }
+    }
+    return g;
+  }
+
+  drawPiece(coord: BoardCoordinate, piece: string): SVGElement {
+    let corner = this.getBoardSVGCord(coord);
+    const DELTA = 3;
+    let g = document.createElementNS(this.xmlns, "g");
+    g.setAttributeNS(
+      null,
+      "transform",
+      `translate(${corner[1] - DELTA},${corner[0] - DELTA})`
+    );
+    g.innerHTML = piece;
+    return g;
+  }
+
+  drawRect(coord: BoardCoordinate): SVGRectElement {
+    let corner = this.getBoardSVGCord(coord);
+    let rect = document.createElementNS(this.xmlns, "rect");
+    rect.setAttributeNS(null, "x", String(corner[1]));
+    rect.setAttributeNS(null, "y", String(corner[0]));
+    rect.setAttributeNS(null, "width", String(this.squareSize));
+    rect.setAttributeNS(null, "height", String(this.squareSize));
+    rect.setAttributeNS(
+      null,
+      "fill",
+      (coord[1] + coord[0]) % 2 === 0 ? this.whiteColor : this.blackColor
+    );
+    return rect;
+  }
+
+  private getBoardSVGCord(coord: BoardCoordinate): [number, number] {
+    return [coord[0] * this.squareSize, coord[1] * this.squareSize];
+  }
+
+  static fromFEN(fenString: string, squareSize: number = 40) {
+    return new SVGChessboard(Chessboard.fromFEN(fenString), squareSize);
+  }
+}

93916b71428ae7cc1301b8be070600749d181c68
Davide Aversa <thek3nger@gmail.com>
2021-03-03 17:29:52 +0100

    feat: ⭐️ add scalability and chessboard coordinates

--- src/index.ts
+++ src/index.ts
@@ -14,26 +14,42 @@ import {
   BLACK_PAWN,
 } from "./Pieces";
 
+export interface SVGChessboardOptions {
+  drawCoordinates: boolean;
+  scale: number;
+}
+
 export class SVGChessboard {
-  chessboard: Chessboard;
+  private chessboard: Chessboard;
+  private squareSize: number;
+  private scale: number;
+
+  private options: SVGChessboardOptions;
 
   private readonly xmlns = "http://www.w3.org/2000/svg";
   private readonly whiteColor = "white";
   private readonly blackColor = "gray";
   private readonly defaultHighlightColor = "green";
+  private readonly baseSquareSize = 40;
 
   private highlights: Array<[BoardCoordinate, string]> = [];
 
   private constructor(
     chessboard: Chessboard,
-    private readonly squareSize: number
+    { drawCoordinates = true, scale = 1 }: Partial<SVGChessboardOptions>
   ) {
     this.chessboard = chessboard;
+    this.scale = scale;
+    this.squareSize = this.baseSquareSize * scale;
+    this.options = { drawCoordinates, scale };
   }
 
   draw(): SVGElement {
     let g = document.createElementNS(this.xmlns, "g");
     g.appendChild(this.drawBoard());
+    if (this.options.drawCoordinates) {
+      g.appendChild(this.drawCoordinateSystem());
+    }
     g.appendChild(this.drawPieces());
     return g;
   }
@@ -123,14 +139,18 @@ export class SVGChessboard {
 
   private drawPiece(coord: BoardCoordinate, piece: string): SVGElement {
     let [x, y] = this.getBoardSVGCord(coord);
-    const DELTA = 3;
+    //const DELTA = 0 * this.scale;
     let g = document.createElementNS(this.xmlns, "g");
-    g.setAttributeNS(null, "transform", `translate(${x - DELTA},${y - DELTA})`);
+    g.setAttributeNS(
+      null,
+      "transform",
+      `translate(${x},${y}) scale(${0.85 * this.scale})`
+    );
     g.innerHTML = piece;
     return g;
   }
 
-  private drawSquare(coord: BoardCoordinate): SVGRectElement {
+  private drawSquare(coord: BoardCoordinate, text?: string): SVGRectElement {
     let [x, y] = this.getBoardSVGCord(coord);
     let rect = document.createElementNS(this.xmlns, "rect");
     rect.setAttributeNS(null, "x", String(x));
@@ -147,14 +167,70 @@ export class SVGChessboard {
         (coord[1] + coord[0]) % 2 === 0 ? this.whiteColor : this.blackColor
       );
     }
+    if (text) {
+    }
     return rect;
+  }
+
+  drawCoordinateSystem(): SVGElement {
+    let g = document.createElementNS(this.xmlns, "g");
+    for (let r = 0; r < 8; r++) {
+      for (let c = 0; c < 8; c++) {
+        if (c === 0) {
+          g.appendChild(this.drawText([c, r], String(8 - r), "row"));
+        }
+        if (r === 7) {
+          g.appendChild(
+            this.drawText([c, r], String(this.numToLetter(c)), "column")
+          );
+        }
+      }
+    }
+    return g;
+  }
+
+  private drawText(
+    [c, r]: BoardCoordinate,
+    text: string,
+    position: "row" | "column"
+  ): SVGElement {
+    let [x, y] = this.getBoardSVGCord([c, r]);
+    let txt = document.createElementNS(this.xmlns, "text");
+    if (position === "row") {
+      txt.setAttributeNS(null, "x", String(x + this.scale * 1));
+      txt.setAttributeNS(null, "y", String(y + this.scale * 10));
+    } else {
+      txt.setAttributeNS(
+        null,
+        "x",
+        String(x + this.squareSize - this.scale * 7)
+      );
+      txt.setAttributeNS(
+        null,
+        "y",
+        String(y + this.squareSize - this.scale * 2)
+      );
+    }
+    txt.setAttributeNS(null, "font-family", "sans-serif");
+    txt.setAttributeNS(null, "font-size", String(this.scale * 10));
+    txt.setAttributeNS(
+      null,
+      "fill",
+      (r + c) % 2 === 0 ? this.blackColor : this.whiteColor
+    );
+    txt.textContent = text;
+    return txt;
+  }
+
+  private numToLetter(num: number): string {
+    return String.fromCharCode(97 + num);
   }
 
   private getBoardSVGCord([c, r]: BoardCoordinate): [number, number] {
     return [c * this.squareSize, r * this.squareSize];
   }
 
-  static fromFEN(fenString: string, squareSize: number = 40) {
-    return new SVGChessboard(Chessboard.fromFEN(fenString), squareSize);
+  static fromFEN(fenString: string, options: Partial<SVGChessboardOptions>) {
+    return new SVGChessboard(Chessboard.fromFEN(fenString), options);
   }
 }
